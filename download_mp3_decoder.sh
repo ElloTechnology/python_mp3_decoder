1a464eddc2e7e7068bb74af41c1c86b9e23e60c2
Joe Lou <joe@joes-mbp.lan>
2023-07-12 16:25:01 +0300

    Add download script

---
+++ download_mp3_decoder.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+curl https://sourceforge.net/projects/swig/files/swig/swig-3.0.12/swig-3.0.12.tar.gz/download
+tar -xzf swig-3.0.12.tar.gz
+cd swig-3.0.12.tar.gz
+curl https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.bz2/download
+mv pcre-8.45.tar.bz2 pcre-8.45.tar
+./Tools/pcre-build.sh
+./autogen.sh
+./configure
+make
+make install
+ln -s $(which swig) $(which swig | sed 's/\[^\/]*$/swig3.0/')
+
+brew install lame
+
